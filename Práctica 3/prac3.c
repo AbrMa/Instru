108cdfb1d984b53434fc02d4278e285ca13c0196
Abraham Manuel Meza Zamora <AbrMa@users.noreply.github.com>
2021-05-25 13:18:19 -0500

    Última etapa digitalización de voltaje

---
+++ Práctica 3/prac3.c
@@ -0,0 +1,22 @@
+#include <16f877a.h> // biblioteca del micro
+#device adc=10 //adc de 10 bits
+#include <stdio.h> //input output c
+#fuses XT, NOWDT, NOPROTECT
+#use delay (clock = 4MHz) // XT = 4MHz
+#use rs232 (baud = 9600, parity = N, xmit = pin_c6, rcv = pin_c7, bits = 8 ) // configuración del puerto serial
+
+void main () {
+   int16 LSB;
+   float voltaje;
+   
+   setup_adc_ports(all_analog);
+   setup_adc(ADC_CLOCK_internal);
+   
+   while (true) {
+      set_adc_channel(0);
+      delay_ms(1000);
+      LSB = read_adc();
+      voltaje = (LSB * 5.0) / 1023.0;
+      printf("Valor en voltaje %2.2f \r\n", voltaje);
+   }
+}
